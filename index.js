40f2c1c492ed7f81808034083861647ad82200fc
Kim-777 <iloo3012@naver.com>
2022-02-06 22:52:24 +0900

    syntax js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-console.log("hi");
+// console.log("hi");
 
 const testObj = {
   a: () => {},
   b: "string",
 };
+
+function test() {
+  return console.log(createSomethingFromRemote());
+}
+
+const createSomethingFromRemote = () => {
+  return true;
+};
+
+test();
